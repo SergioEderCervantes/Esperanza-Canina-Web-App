e8e7e22aa7f6fc5546d4b9db8c3d98a4c39cc1fb
StoneyGravy963 <al210356@edu.uaa.mx>
2025-09-25 01:54:11 -0600

    Arreglao

--- backend/app/dogs_api/admin.py
+++ backend/app/dogs_api/admin.py
@@ -1,47 +1,96 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group, User
+from unfold.admin import ModelAdmin
 from django.contrib.admin import SimpleListFilter
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from django.utils.html import format_html
 
-from app.dogs_api import models
+from app.dogs_api.models import Dog, DogImage
+
+# Acomodar los base models de User y Groups
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
-class AdoptionStateFilter(SimpleListFilter):
-    title = "Estado de adopción"
-    parameter_name = "adoption_state"
-
-    def lookups(self, request, model_admin):
-        return (
-            ("1", "Adoptados"),
-            ("0", "No Adoptados"),
-        )
-
-    def queryset(self, request, queryset):
-        if self.value() == "1":
-            return queryset.filter(adoption_state=True)
-        if self.value() == "0":
-            return queryset.filter(adoption_state=False)
-        return queryset
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    # Forms loaded from `unfold.forms`
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+    icon = "person"
 
 
-class DogAdmin(admin.ModelAdmin):
-    list_display = ("name", "dog_life_stage", "adoption_state", "view_image_button")
-    list_editable = ("adoption_state",)
-    list_filter = (AdoptionStateFilter,)
-    exclude = ("adoption_state",)
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    icon = "group"
+    pass
 
-    def view_image_button(self, obj):
-        if obj.primary_image() is not None:
+class DogImageInline(admin.TabularInline):
+    model = DogImage
+    extra = 1
+    fields = ("image_thumbnail", "is_primary")
+    readonly_fields = ("image_thumbnail",)
+    show_change_link = True
+
+    def image_thumbnail(self, obj):
+        if obj.image:
             return format_html(
-                '<img src="{}" width="70" style="margin-right:8px; border-radius:4px;" />'
-                '<a href="{}" target="_blank">Ampliar</a>',
-                obj.primary_image(),
-                obj.primary_image(),
+                '<a href="{0}" target="_blank"><img src="{0}" style="height: 80px;"/></a>',
+                obj.image.url
             )
-        return "Sin foto"
+        return "-"
+    image_thumbnail.short_description = "Imagen"
 
-    view_image_button.short_description = "Imagen"
+@admin.register(Dog)
+class DogAdminCLass(ModelAdmin):
+    inlines = [DogImageInline]
+    list_display = ("name", "adoption_state_display", "primary_image_thumbnail")
+    search_fields = ("name",)
+    list_filter = ("adoption_state",)
 
+    def adoption_state_display(self, obj):
+        return "Adoptado" if obj.adoption_state else "No adoptado"
+    adoption_state_display.short_description = "Estado de adopción"
 
-admin.site.register(models.Beheavior)
-admin.site.register(models.DogImage)
-admin.site.register(models.Dog, DogAdmin)
+    def primary_image_thumbnail(self, obj):
+        url = obj.primary_image()
+        if url:
+            return format_html(
+                '<a href="{0}" target="_blank"><img src="{0}" style="height: 80px;"/></a>',
+                url
+            )
+        return "-"
+    primary_image_thumbnail.short_description = "Imagen primaria"
+    
+@admin.register(DogImage)
+class DogImageAdmin(ModelAdmin):
+    list_display = ("dog_id", "dog", "image_thumbnail", "is_primary")
+    search_fields = ("dog__name",)
+    list_filter = ("dog",)
+    list_display_links = ("dog",)
+    readonly_fields = ("image_preview",)
+    fields = ("dog", "image_preview", "is_primary") 
+
+    def image_thumbnail(self, obj):
+        if obj.image:
+            return format_html(
+                '<a href="{0}" target="_blank"><img src="{0}" style="height: 80px;"/></a>',
+                obj.image.url
+            )
+        return "-"
+    image_thumbnail.short_description = "Imagen"
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{0}" style="height: 200px;"/>',
+                obj.image.url
+            )
+        return "-"
+    image_preview.short_description = "Vista previa"
+    
+    
+    
