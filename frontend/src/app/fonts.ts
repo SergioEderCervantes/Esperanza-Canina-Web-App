445c04f8c4ffedd078260e2d4026b536f4893f74
DJoztin <al267187@edu.uaa.mx>
2025-12-18 01:50:18 -0600

    cambio fuente y actu next

--- frontend/src/app/fonts.ts
+++ frontend/src/app/fonts.ts
@@ -3,15 +3,11 @@ import localFont from 'next/font/local';
 export const myFont = localFont({
   src: [
     {
-      path: '../../public/fonts/Typo_Round_Regular_Demo.otf',
-      weight: '400',
+      path: '../../public/fonts/RockoFLF.ttf',
+      weight: '200',
       style: 'normal',
     },
-    {
-      path: '../../public/fonts/Typo_Round_Bold_Demo.otf',
-      weight: '700',
-      style: 'normal',
-    },
+    
   ],
   variable: '--font-myFont',
 });
--- frontend/src/components/layout/Nav.tsx
+++ frontend/src/components/layout/Nav.tsx
@@ -140,7 +140,7 @@ export default function Nav() {
             </Link>
             <Link
               href="/perritos"
-              className="px-5 py-2 rounded-full bg-blue-400 text-white font-bold shadow-md hover:bg-blue-600 transition 
+              className="px-5 py-2 rounded-full bg-blue-400 text-white shadow-md hover:bg-blue-600 transition 
                 duration-300"
               onClick={toggleMobileMenu}
             >
@@ -148,7 +148,7 @@ export default function Nav() {
             </Link>
             <Link
               href="/ayuda"
-              className="px-5 py-2 rounded-full bg-blue-400 text-white font-bold shadow-md hover:bg-blue-600 transition 
+              className="px-5 py-2 rounded-full bg-blue-400 text-white shadow-md hover:bg-blue-600 transition 
                  duration-300"
               onClick={toggleMobileMenu}
             >
