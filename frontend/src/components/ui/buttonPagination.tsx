f7fb8898cb2c0077c5d6d5396100d6c983302617
DJoztin <al267187@edu.uaa.mx>
2025-12-22 17:08:54 -0600

    nada

--- frontend/src/components/ui/buttonPagination.tsx
+++ frontend/src/components/ui/buttonPagination.tsx
@@ -13,12 +13,22 @@ export function ButtonPagination({
   totalPages,
   onChangePage,
 }: ButtonPaginationProps) {
+
+  const handleChangePage = (page: number) => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    onChangePage(page);
+  };
+
   return (
     <div className="flex gap-2 items-center">
       <button
         disabled={currentPage === 1}
-        onClick={() => onChangePage(currentPage - 1)}
-        className="px-1 py-1 rounded-lg bg-transparent disabled:opacity-50 text-white"
+        onClick={() => handleChangePage(currentPage - 1)}
+        className="px-1 py-1 rounded-lg bg-transparent disabled:opacity-50"
       >
         <ChevronLeft size={24} className="text-blue-500" />
       </button>
@@ -28,11 +38,11 @@ export function ButtonPagination({
         return (
           <button
             key={num}
-            onClick={() => onChangePage(num)}
+            onClick={() => handleChangePage(num)}
             className={`px-3 text-lg bg-transparent ${
               currentPage === num
-                ? " text-blue-500 text-xl font-bold"
-                : " text-black hover:text-blue-400"
+                ? "text-blue-500 text-xl font-bold"
+                : "text-black hover:text-blue-400"
             }`}
           >
             {num}
@@ -42,8 +52,8 @@ export function ButtonPagination({
 
       <button
         disabled={currentPage === totalPages}
-        onClick={() => onChangePage(currentPage + 1)}
-        className="px-2 py-2 rounded-lg bg-transparent disabled:opacity-50 text-white"
+        onClick={() => handleChangePage(currentPage + 1)}
+        className="px-2 py-2 rounded-lg bg-transparent disabled:opacity-50"
       >
         <ChevronRight size={24} className="text-blue-500" />
       </button>
